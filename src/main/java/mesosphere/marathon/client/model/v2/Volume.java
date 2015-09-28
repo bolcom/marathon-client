b168c6f7a1f80702a2edfaf2cb425854e4acf1f0
Jeroen Wijdemans <jwijdemans@bol.com>
2015-09-28 16:42:56 +0200

    Added volumes
    Change-Id: I930f9d755e65e2449c4feddfd2bebb87a0e706cd

--- src/main/java/mesosphere/marathon/client/model/v2/Volume.java
+++ src/main/java/mesosphere/marathon/client/model/v2/Volume.java
@@ -1,5 +1,57 @@
 package mesosphere.marathon.client.model.v2;
 
+/**
+ * {
+ * "containerPath": "/etc/a",
+ * "hostPath": "/var/data/a",
+ * "mode": "RO"
+ * }
+ */
 public class Volume {
 
+    private String containerPath;
+    private String hostPath;
+    private String mode;
+
+    public Volume() {
+    }
+
+    public Volume(String containerPath, String hostPath, String mode) {
+        this.containerPath = containerPath;
+        this.hostPath = hostPath;
+        this.mode = mode;
+    }
+
+    public String getContainerPath() {
+        return containerPath;
+    }
+
+    public void setContainerPath(String containerPath) {
+        this.containerPath = containerPath;
+    }
+
+    public String getHostPath() {
+        return hostPath;
+    }
+
+    public void setHostPath(String hostPath) {
+        this.hostPath = hostPath;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    @Override
+    public String toString() {
+        return "Volume{" +
+                "containerPath='" + containerPath + '\'' +
+                ", hostPath='" + hostPath + '\'' +
+                ", mode='" + mode + '\'' +
+                '}';
+    }
 }
